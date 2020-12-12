b46f066faf9baae75c03cff19b299ab41421c7fa
Romano Livi <romano_livi@outlook.com>
2020-12-12 18:33:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,13 +5,14 @@ def reverse_each_word(sentence)
 end
 
 
-def reverse_each_word(sentence)
+def reverse_each_word_each(sentence)
   reversed = ""
   sentence.split.each {|word| reversed << "#{word.reverse} "}
   return reversed.strip
 end
 
-
+puts reverse_each_word_each("Hey Buddy")
+puts reverse_each_word("Hey Buddy")
 
 
 
