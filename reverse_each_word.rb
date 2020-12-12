7fc3867a52918c6a481a7dfc7b4db42b03dc6bd5
Romano Livi <romano_livi@outlook.com>
2020-12-12 18:19:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   reversed = ''
-  sentence.split.each {|word| reversed << "#{word.reverse}"}
+  sentence.split.each {|word| reversed << "#{word.reverse} "}
   return reversed
 end
 
